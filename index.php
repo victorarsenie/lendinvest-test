39be1b1703b493374f39d0f827fc8168b449d72d
victorarsenie <victor.arsenie@yahoo.com>
2019-08-31 15:07:19 +0100

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$start_date = "01/10/2015";
-$end_date = "15/11/2015";
-$tranch_a_interest = "3%";
-$tranch_b_interest = "6%";
-
-
-$loan = ["start_date", "end_Date", "tranch_a", "tranch_b", ];
-?>
